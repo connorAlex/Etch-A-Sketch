f6048556d54cd9253add5d63fa4b8fd83f628b6e
connorAlex <alex.connor.cochrane@gmail.com>
2022-05-25 16:33:42 -0500

    fix container strech bug

--- EAS.js
+++ EAS.js
@@ -1,17 +1,56 @@
 //set up a 16x16 grid of squares
 
 const container = document.querySelector(".container");
-
-//get slider value
 let range = document.querySelector(".slider");
 let label = document.querySelector(".label");
-//set label to slider value; default is 16.
-label.innerHTML = range.value;
+
 let mydim = range.value;
+let btn = document.querySelector(".reset");
+let cont = document.querySelector(".container");
 
 setDimensions(mydim);
 
-range.oninput  = function () {
+cont.addEventListener("mouseover", draw);
+cont.addEventListener("touchmove", draw);
+
+label.innerHTML = range.value;
+range.oninput  = adjustRange;
+
+btn.addEventListener("click", clear);
+
+function setDimensions(dim){
+    //create rows
+    for (i = 0; i < dim; i ++){
+        const row = document.createElement("div");
+        row.className = "row";
+        container.appendChild(row);
+        //set up squares
+        for (j = 0; j < dim; j ++){
+            const square = document.createElement("div");
+            square.className = "square";
+            square.style.padding = `${400 / dim}px`;       
+            row.appendChild(square);
+        }   
+    }
+}
+
+function draw(e){
+    let rainbow = document.querySelector(".rainbow").checked;
+    let eraser = document.querySelector(".eraser").checked;
+    if ( e.target.className === "square"){
+        if (rainbow === true){
+            e.target.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+        }
+        else if (eraser === true){
+            e.target.style.backgroundColor = "#FBF7F4";
+        }
+        else{
+            e.target.style.backgroundColor = "#6C9A8B";
+        }
+    }
+}
+
+function adjustRange(){
     label.innerHTML = this.value;
     mydim = this.value;
     while (container.firstChild) { 
@@ -20,10 +59,7 @@ range.oninput  = function () {
     setDimensions(mydim);
 }
 
-
-//add reset click listener
-let btn = document.querySelector(".reset");
-btn.addEventListener("click", function(e) {
+function clear(){
     let rows = container.childNodes;
     rows.forEach(function (e){
         e.childNodes.forEach(function(s){
@@ -31,42 +67,4 @@ btn.addEventListener("click", function(e) {
         });
     });
     console.log("asdf");
-});
-
-//add mouse click listener
-let cont = document.querySelector(".container");
-cont.addEventListener("mouseover", draw);
-cont.addEventListener("touchstart");
-function setDimensions(dim){
- 
-    //create rows
-    for (i = 0; i < dim; i ++){
-        const row = document.createElement("div");
-        row.className = "row";
-        container.appendChild(row);
-
-        //set up squares
-        for (j = 0; j < dim; j ++){
-            const square = document.createElement("div");
-            square.className = "square";
-            square.style.padding = `${400 / dim}px`;       //spacing must be relative to the dimensions
-        
-            row.appendChild(square);
-            
-        }   
-    }
-}
-
-function draw(e){
-    let rainbow = document.querySelector(".rainbow").checked;
-    if ( e.target.className === "square"){
-        if (rainbow === true){
-            
-            e.target.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-        }
-        else{
-            e.target.style.backgroundColor = "#6C9A8B";
-        }
-        
-    }
 }
